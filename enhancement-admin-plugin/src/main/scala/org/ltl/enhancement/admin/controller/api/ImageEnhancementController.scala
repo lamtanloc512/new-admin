bccc2aa8eb134cd8183c0f9323faa9b63ff64186
lamtanloc512 <lamtanloc512@gmail.com>
2025-09-15 20:56:15 +0700

    continue

--- enhancement-admin-plugin/src/main/scala/org/ltl/enhancement/admin/controller/api/ImageEnhancementController.scala
+++ enhancement-admin-plugin/src/main/scala/org/ltl/enhancement/admin/controller/api/ImageEnhancementController.scala
@@ -14,15 +14,21 @@ import org.youngmonkeys.ezyplatform.model.PaginationModel
 import org.youngmonkeys.ezyplatform.response.MediaResponse
 import org.youngmonkeys.ezyplatform.util.StringConverters
 import org.youngmonkeys.ezyplatform.pagination.DefaultMediaFilter
+import com.tvd12.ezyhttp.core.response.ResponseEntity
+import com.tvd12.ezyhttp.server.core.request.RequestArguments
+import org.ltl.enhancement.admin.service.ImageEnhancementService
+import org.youngmonkeys.ezyplatform.model.MediaModel
+import org.youngmonkeys.ezyplatform.exception.MediaNotFoundException
+import java.io.File
 
 @Api
 @Authenticated
 @Controller("/api/v1")
 @EzyFeature("enhancement_admin")
 class ImageEnhancementController @EzyAutoBind() (
-    fileSystemManager: FileSystemManager,
     adminService: AdminAdminService,
-    mediaControllerService: AdminMediaControllerService
+    mediaControllerService: AdminMediaControllerService,
+    imageEnhancementService: ImageEnhancementService
 ) {
 
   @DoGet("/media/list")
@@ -61,81 +67,17 @@ class ImageEnhancementController @EzyAutoBind() (
     else adminService.isAllowAccessAllMedia(adminId)
   }
 
-  // @DoGet("/media/{id}/details")
-  // def getMediaDetails(@PathVariable("id") id: Long): ResponseEntity = {
-  //   try {
-  //     // For now, return placeholder data
-  //     // In a real implementation, you would query the database by ID
-  //     val mediaDetails = AdminMediaDetailsResponse(
-  //       id = id,
-  //       name = s"media_$id",
-  //       originalName = s"original_media_$id.jpg",
-  //       mimeType = "image/jpeg",
-  //       size = 1024000L,
-  //       width = 1920,
-  //       height = 1080,
-  //       url = s"/api/v1/media/media_$id.jpg",
-  //       publicMedia = true,
-  //       `type` = "IMAGE",
-  //       alternativeText = "",
-  //       title = "",
-  //       caption = "",
-  //       description = "",
-  //       createdAt = System.currentTimeMillis(),
-  //       ownerAdmin = Some(AdminInfo("admin", "Administrator"))
-  //     )
-
-  //     ResponseEntity.ok(AdminMediaDetailsResponse.toJava(mediaDetails))
-
-  //   } catch {
-  //     case e: Exception =>
-  //       ResponseEntity.badRequest(
-  //         Map("error" -> s"Failed to get media details: ${e.getMessage}").asJava
-  //       )
-  //   }
-  // }
-
-  // @DoGet("/media/{name}")
-  // def getMediaFile(@PathVariable("name") name: String): ResponseEntity = {
-  //   try {
-  //     // Try processed file first, then original
-  //     val processedFile = new File(s"$processedStorageDir/$name")
-  //     val originalFile = new File(s"$mediaStorageDir/$name")
-
-  //     val fileToServe =
-  //       if (processedFile.exists()) processedFile else originalFile
-
-  //     if (!fileToServe.exists()) {
-  //       return ResponseEntity.notFound()
-  //     }
-
-  //     // Read file bytes
-  //     val fileBytes = Files.readAllBytes(fileToServe.toPath)
-
-  //     // Determine content type
-  //     val contentType = Files.probeContentType(fileToServe.toPath)
-  //     val finalContentType =
-  //       if (contentType != null) contentType else "application/octet-stream"
-
-  //     ResponseEntity.ok(fileBytes)
-
-  //   } catch {
-  //     case e: Exception =>
-  //       e.printStackTrace()
-  //       ResponseEntity.notFound()
-  //   }
-  // }
-
-  // @DoPut("/media/{id}")
-  // def updateMedia(
-  //     @PathVariable("id") id: Long,
-  //     @RequestBody request: java.util.Map[String, Any]
-  // ): ResponseEntity = {
-  //   result match {
-  //     case Right(response) => response
-  //     case Left(error) =>
-  //       ResponseEntity.badRequest(Map("error" -> error).asJava)
-  //   }
-  // }
+  @DoGet("/media/{name}")
+  def getMediaFile(
+      args: RequestArguments,
+      @PathVariable name: String
+  ): Unit = {
+    imageEnhancementService.getCompressedMedia(
+      args,
+      name,
+      exposePrivateMedia = true,
+      _ => true
+    )
+  }
 
 }
--- enhancement-admin-plugin/src/main/scala/org/ltl/enhancement/admin/service/ImageEnhancementService.scala
+++ enhancement-admin-plugin/src/main/scala/org/ltl/enhancement/admin/service/ImageEnhancementService.scala
@@ -2,13 +2,15 @@ package org.ltl.enhancement.admin.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.sksamuel.scrimage.ImmutableImage
+import com.sksamuel.scrimage.format.{Format, FormatDetector}
+import com.sksamuel.scrimage.nio.{GifWriter, ImageWriter, JpegWriter, PngWriter}
 import com.sksamuel.scrimage.webp.WebpWriter
 import com.tvd12.ezyfox.bean.EzySingletonFactory
 import com.tvd12.ezyfox.bean.annotation.{EzyAutoBind, EzySingleton}
 import com.tvd12.ezyfox.concurrent.EzyLazyInitializer
 import com.tvd12.ezyfox.stream.EzyInputStreamLoader
+import com.tvd12.ezyfox.util.EzyLoggable
 import com.tvd12.ezyhttp.client.HttpClient
-import com.tvd12.ezyhttp.core.exception.HttpNotAcceptableException
 import com.tvd12.ezyhttp.core.resources.ResourceDownloadManager
 import com.tvd12.ezyhttp.server.core.handler.ResourceRequestHandler
 import com.tvd12.ezyhttp.server.core.request.RequestArguments
@@ -18,8 +20,6 @@ import org.youngmonkeys.ezyplatform.converter.{
   HttpModelToResponseConverter,
   HttpRequestToModelConverter
 }
-import org.youngmonkeys.ezyplatform.data.FileMetadata
-import org.youngmonkeys.ezyplatform.entity.{MediaType, UploadFrom}
 import org.youngmonkeys.ezyplatform.event.*
 import org.youngmonkeys.ezyplatform.exception.MediaNotFoundException
 import org.youngmonkeys.ezyplatform.io.FolderProxy
@@ -28,14 +28,13 @@ import org.youngmonkeys.ezyplatform.model.*
 import org.youngmonkeys.ezyplatform.service.*
 import org.youngmonkeys.ezyplatform.validator.{CommonValidator, MediaValidator}
 
-import scala.jdk.CollectionConverters.*
-import scala.util.Try
-import java.io.File
-import java.util.function.Predicate
-import javax.servlet.http
+import java.io.{File, FileInputStream}
+import java.nio.file.Files
+import javax.servlet.http.HttpServletResponse
+import scala.util.{Try, Using}
 
 @EzySingleton
-class MediaControllerService @EzyAutoBind() (
+class ImageEnhancementService @EzyAutoBind() (
     httpClient: HttpClient,
     eventHandlerManager: EventHandlerManager,
     fileSystemManager: FileSystemManager,
@@ -50,7 +49,7 @@ class MediaControllerService @EzyAutoBind() (
     singletonFactory: EzySingletonFactory,
     inputStreamLoader: EzyInputStreamLoader,
     objectMapper: ObjectMapper
-) {
+) extends EzyLoggable {
 
   private val fileUploaderWrapper =
     new EzyLazyInitializer(() =>
@@ -60,168 +59,98 @@ class MediaControllerService @EzyAutoBind() (
   private val tika =
     new EzyLazyInitializer(() => TikaConfig())
 
-  def addMedia(
-      request: http.HttpServletRequest,
-      response: http.HttpServletResponse,
-      uploadFrom: UploadFrom,
-      ownerId: Long,
-      avatar: Boolean,
-      notPublic: Boolean
-  ): Unit = {
-    val fileUploader = fileUploaderWrapper.get()
-    if (fileUploader == null)
-      throw new HttpNotAcceptableException(
-        Map("fileUpload" -> "disabled").asJava
-      )
-
-    val filePartEither = Option(request.getPart("file"))
-      .orElse(request.getParts.asScala.headOption) match {
-      case Some(fp) => Right(fp)
-      case None =>
-        Left(new HttpNotAcceptableException(Map("file" -> "missing").asJava))
-    }
-
-    filePartEither match {
-      case Right(filePart) =>
-        val fileMetadataEither = Try(
-          mediaValidator.validateFilePart(filePart, avatar)
-        ).toEither.left.map(e =>
-          new HttpNotAcceptableException(Map("file" -> e.getMessage).asJava)
-        )
-
-        fileMetadataEither match {
-          case Right(fileMetadata) =>
-            eventHandlerManager.handleEvent(
-              new MediaUploadEvent(uploadFrom, ownerId, fileMetadata)
-            )
-
-            val submittedFileName = filePart.getSubmittedFileName
-            val extension = fileMetadata.getExtension
-            val containerFolder = fileMetadata.getMediaType.getFolder
-            val newFileName =
-              mediaService.generateMediaFileName(submittedFileName, extension)
-
-            val asyncContext = request.getAsyncContext
-            val mediaFilePath =
-              fileSystemManager.getMediaFilePath(containerFolder, newFileName)
-
-            fileUploader.accept(
-              asyncContext,
-              filePart,
-              mediaFilePath,
-              settingService.getMaxUploadFileSize,
-              () => {
-                val finalFile = if (
-                  fileMetadata.getMediaType == MediaType.IMAGE || fileMetadata.getMediaType == MediaType.AVATAR
-                ) {
-                  convertToWebp(mediaFilePath) match {
-                    case Right(webpFile) => webpFile
-                    case Left(e) =>
-                      println(s"WebP conversion failed: ${e.getMessage}")
-                      mediaFilePath
-                  }
-                } else mediaFilePath
-
-                val model = saveMediaInformation(
-                  uploadFrom,
-                  ownerId,
-                  submittedFileName,
-                  newFileName,
-                  fileMetadata,
-                  notPublic
-                )
-                notifyMediaEvent(
-                  new MediaUploadedEvent(model, finalFile)
-                ) match {
-                  case Right(_) => ()
-                  case Left(e) =>
-                    println(
-                      s"[WARN] notify media event failed: ${e.getMessage}"
-                    )
-                }
-                val respBytes = objectMapper.writeValueAsBytes(model)
-                response.getOutputStream.write(respBytes)
-              }
-            )
-          case Left(e) => throw e
-        }
-      case Left(e) => throw e
-    }
-  }
-
-  private def convertToWebp(originalFile: File): Either[Throwable, File] = {
-    Try {
-      val img = ImmutableImage.loader().fromFile(originalFile)
-      val webpFile = new File(
-        originalFile.getParent,
-        originalFile.getName.replaceAll("\\.[^.]+$", "") + ".webp"
-      )
-      img.output(WebpWriter.MAX_LOSSLESS_COMPRESSION, webpFile)
-      originalFile.delete()
-      webpFile
-    }.toEither
-  }
-
-  def getMedia(
+  def getCompressedMedia(
       requestArguments: RequestArguments,
       name: String,
       exposePrivateMedia: Boolean,
-      validMediaCondition: Predicate[MediaModel]
-  ): Either[MediaNotFoundException, Unit] = {
-    mediaValidator.validateMediaName(name)
-    Option(mediaService.getMediaByName(name)) match {
-      case Some(media)
-          if media.isPublicMedia || exposePrivateMedia || validMediaCondition
-            .test(media) =>
-        notifyMediaEvent(new MediaDownloadEvent(media)) match {
-          case Right(_) => ()
-          case Left(e) =>
-            println(s"[WARN] notify media event failed: ${e.getMessage}")
-        }
-        val mediaType = media.getType
+      validMediaCondition: java.util.function.Predicate[MediaModel]
+  ): Either[Throwable, Unit] = {
 
-        convertToWebp(
-          fileSystemManager.getMediaFilePath(mediaType.getFolder, name)
-        )
-          .fold(
-            err => Left(MediaNotFoundException(err.getMessage)),
-            webp => {
-              val extension = FolderProxy.getFileExtension(name)
-              val handler = new ResourceRequestHandler(
-                webp.toString,
-                webp.toString,
-                extension,
-                inputStreamLoader,
-                resourceDownloadManager
-              )
-              handler.handle(requestArguments)
-              Right(())
-            }
+    def writerFor(format: Format): ImageWriter = format match {
+      case Format.PNG  => PngWriter.MaxCompression
+      case Format.GIF  => GifWriter.Default
+      case Format.JPEG => JpegWriter.Default
+      case Format.WEBP => WebpWriter.DEFAULT
+    }
+
+    def detectFormat(file: File): Either[Throwable, Format] =
+      Using(new FileInputStream(file)) { in =>
+        FormatDetector.detect(in).orElse(Format.JPEG)
+      }.toEither
+
+    def compressAndCache(
+        original: File,
+        cached: File
+    ): Either[Throwable, File] =
+      Try {
+        val image = ImmutableImage.loader().fromFile(original)
+        cached.getParentFile.mkdirs()
+        detectFormat(original) match {
+          case Left(error) => new MediaNotFoundException(error.getMessage)
+          case Right(format) =>
+            val writer = writerFor(format)
+            image.output(writer, cached)
+        }
+        cached
+      }.toEither
+
+    for {
+      _ <- Try(mediaValidator.validateMediaName(name)).toEither
+
+      media <- Option(mediaService.getMediaByName(name))
+        .toRight(new MediaNotFoundException(s"Media not found: $name"))
+
+      _ <- Either.cond(
+        media.isPublicMedia || exposePrivateMedia || validMediaCondition.test(
+          media
+        ),
+        (),
+        new MediaNotFoundException(s"Media not accessible: $name")
+      )
+
+      resourceFile <- Try(
+        fileSystemManager.getMediaFilePath(media.getType.getFolder, name)
+      ).toEither
+        .flatMap(f =>
+          Either.cond(
+            f.exists(),
+            f,
+            new MediaNotFoundException(s"File not found on disk: $name")
           )
-      case _ => Left(new MediaNotFoundException(name))
+        )
+
+      uploadFolder <- Try(fileSystemManager.getUploadFolder).toEither
+      compressedFile = new File(uploadFolder, s"images/compressed/$name")
+
+      cachedFile <-
+        if (compressedFile.exists()) Right(compressedFile)
+        else compressAndCache(resourceFile, compressedFile)
+
+      _ <- Try {
+        val response: HttpServletResponse = requestArguments.getResponse
+        val mimeType = Files.probeContentType(cachedFile.toPath) match {
+          case null => "application/octet-stream"
+          case mt   => mt
+        }
+        logger.info("Image path: {}", cachedFile.toPath)
+        response.setContentType(mimeType)
+
+        val handler = new ResourceRequestHandler(
+          cachedFile.toString,
+          cachedFile.toString,
+          FolderProxy.getFileExtension(name),
+          inputStreamLoader,
+          resourceDownloadManager
+        )
+        handler.handle(requestArguments)
+
+      }.toEither
+
+    } yield {
+      notifyMediaEvent(new MediaDownloadEvent(media))
+      ()
     }
   }
-
-  private def saveMediaInformation(
-      uploadFrom: UploadFrom,
-      ownerId: Long,
-      submittedFileName: String,
-      fileName: String,
-      fileMetadata: FileMetadata,
-      notPublic: Boolean
-  ) =
-    mediaService.addMedia(
-      AddMediaModel
-        .builder()
-        .ownerId(ownerId)
-        .fileName(fileName)
-        .originalFileName(submittedFileName)
-        .mediaType(fileMetadata.getMediaType)
-        .mimeType(fileMetadata.getMimeType)
-        .notPublic(notPublic)
-        .build(),
-      uploadFrom
-    )
 
   private def notifyMediaEvent(event: Any): Either[Throwable, Unit] =
     Try(eventHandlerManager.handleEvent(event)).toEither
